036ce6693cdacf828e209443105af988596ab60e
Ryan-Millard <millardryandevon@gmail.com>
2024-11-06 11:28:13 +0200

    Improve SQL for Wishlist table

--- app/Seeders/WishlistSeeder.php
+++ app/Seeders/WishlistSeeder.php
@@ -11,13 +11,21 @@ class WishlistSeeder extends Seeder {
 		parent::__construct();
 		$this->tableName = 'Wishlist';
 		$this->tableStructure = '
-			`wishlist_item_id` int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+			`wishlist_item_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 			`buyer_id` int(11) UNSIGNED NOT NULL,
 			`product_id` int(11) UNSIGNED NOT NULL,
-			`quantity` int(11) NOT NULL,
+			`quantity` int(11) UNSIGNED NOT NULL,
+			PRIMARY KEY (`wishlist_item_id`),
 			UNIQUE KEY `unique_wishlist_item` (`buyer_id`, `product_id`),
-			FOREIGN KEY (`buyer_id`) REFERENCES `Buyers` (`buyer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-			FOREIGN KEY (`product_id`) REFERENCES `Products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+			KEY `product_id` (`product_id`),
+			CONSTRAINT `Wishlist_ibfk_1`
+				FOREIGN KEY (`buyer_id`)
+				REFERENCES `Buyers` (`buyer_id`)
+				ON DELETE CASCADE ON UPDATE CASCADE,
+			CONSTRAINT `Wishlist_ibfk_2`
+				FOREIGN KEY (`product_id`)
+				REFERENCES `Products` (`product_id`)
+				ON DELETE CASCADE ON UPDATE CASCADE
 		';
 		$this->columnTypes = 'iiii';
 		$this->seedFile = 'wishlistData.txt';
